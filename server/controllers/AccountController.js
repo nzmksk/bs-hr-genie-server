6c4735a064044f3a09ed35cf4c9c1641cab2bb5f
nzmksk <mhafiz9676@gmail.com>
2023-06-30 03:26:06 +0800

    Add logout function

--- server/controllers/AccountController.js
+++ server/controllers/AccountController.js
@@ -106,7 +106,13 @@ const loginAccount = async (request, response) => {
   }
 };
 
+const logoutAccount = async (request, response) => {
+  response.clearCookie("hr-genie", { path: "/refresh_token" });
+  return response.status(200).json({ message: "Logout successful." });
+};
+
 module.exports = {
   registerNewEmployee,
   loginAccount,
+  logoutAccount,
 };
--- server/routes/AccountRouter.js
+++ server/routes/AccountRouter.js
@@ -4,6 +4,7 @@ const accountController = require("../controllers/AccountController.js");
 const router = Router();
 
 router.post("/login", accountController.loginAccount);
+router.post("/logout", accountController.logoutAccount);
 router.post("/register", accountController.registerNewEmployee);
 
 module.exports = router;
