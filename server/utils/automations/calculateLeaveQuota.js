89bf861d525cfe9e672552e626fc091964d4c8ec
nzmksk <mhafiz9676@gmail.com>
2023-07-01 23:11:36 +0800

    Add calculateLeaveQuota function

---
+++ server/utils/automations/calculateLeaveQuota.js
@@ -0,0 +1,131 @@
+const calculateLeaveQuota = (leaveType, role, tenure) => {
+  let leaveQuota;
+
+  switch (leaveType) {
+    // Annual leave allocation
+    case 1:
+      switch (role) {
+        case "manager":
+          switch (tenure) {
+            // Tenure < 1 year
+            case 0:
+              // TODO: Prorate it
+              break;
+
+            // Tenure 1-2 years
+            case 1:
+              leaveQuota = 12;
+              break;
+
+            // Tenure 2-5 years
+            case 2:
+            case 3:
+            case 4:
+              leaveQuota = 16;
+              break;
+
+            // Tenure 5+ years
+            default:
+              leaveQuota = 20;
+              break;
+          }
+          break;
+
+        case "admin":
+        case "employee":
+          switch (tenure) {
+            // Tenure < 1 year
+            case 0:
+              // TODO: Prorate it
+              break;
+
+            // Tenure 1-2 years
+            case 1:
+              leaveQuota = 8;
+              break;
+
+            // Tenure 2-5 years
+            case 2:
+            case 3:
+            case 4:
+              leaveQuota = 12;
+              break;
+
+            // Tenure 5+ years
+            default:
+              leaveQuota = 16;
+              break;
+          }
+
+        default:
+          break;
+      }
+      break;
+
+    // Medical leave allocation
+    case 2:
+      switch (role) {
+        case "manager":
+          switch (tenure) {
+            // Tenure < 1 year
+            case 0:
+              // TODO: Prorate it
+              break;
+
+            // Tenure 1-2 years
+            case 1:
+              leaveQuota = 16;
+              break;
+
+            // Tenure 2-5 years
+            case 2:
+            case 3:
+            case 4:
+              leaveQuota = 18;
+              break;
+
+            // Tenure 5+ years
+            default:
+              leaveQuota = 22;
+              break;
+          }
+          break;
+
+        case "admin":
+        case "employee":
+          switch (tenure) {
+            // Tenure < 1 year
+            case 0:
+              // TODO: Prorate it
+              break;
+
+            // Tenure 1-2 years
+            case 1:
+              leaveQuota = 14;
+              break;
+
+            // Tenure 2-5 years
+            case 2:
+            case 3:
+            case 4:
+              leaveQuota = 16;
+              break;
+
+            // Tenure 5+ years
+            default:
+              leaveQuota = 18;
+              break;
+          }
+
+        default:
+          break;
+      }
+
+    default:
+      break;
+  }
+
+  return leaveQuota;
+};
+
+module.exports = calculateLeaveQuota;
